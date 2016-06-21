33ccba1a52cf81db5ddad73163b01d6854de918c
Jake Goulding <jake.goulding@integer32.com>
2016-06-21 19:04:04 -0400

    Use ACE editor

--- ui/Editor.jsx
+++ ui/Editor.jsx
@@ -1,11 +1,24 @@
 import React, { PropTypes } from 'react';
+import AceEditor from 'react-ace';
+import brace from 'brace';
+
+import 'brace/mode/rust';
+import 'brace/theme/github';
+import 'brace/keybinding/emacs';
 
 export default class Editor extends React.Component {
   render() {
     const { code, onEditCode } = this.props;
 
     return (
-      <textarea value={ code } onChange={ (e) => onEditCode(e.target.value) } />
+      <AceEditor
+         mode="rust"
+         theme="github"
+         keyboardHandler="emacs"
+         value={ code }
+         onChange={ onEditCode }
+         name="editor"
+         editorProps={ { $blockScrolling: true } } />
     );
   }
 };
