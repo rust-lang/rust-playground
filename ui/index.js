373b31a058e048168865bca56d8b28b514b0f57b
Jake Goulding <jake.goulding@integer32.com>
2016-06-23 00:24:00 -0400

    Store the code in localstorage

--- ui/index.js
+++ ui/index.js
@@ -4,18 +4,37 @@ promisePolyfill.polyfill();
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import persistState from 'redux-localstorage';
 
 import playgroundApp from './reducers';
 import Playground from './Playground.jsx';
 
+const CURRENT_VERSION = 1;
+
+function serialize(state) {
+  return JSON.stringify({
+    version: CURRENT_VERSION,
+    code: state.code
+  });
+}
+
+function deserialize(savedState) {
+  if (!savedState) { return undefined; }
+  const parsedState = JSON.parse(savedState);
+  if (parsedState.version != CURRENT_VERSION) { return undefined; }
+
+  return {
+    code: parsedState.code
+  };
+}
+
 const logger = createLogger(); // TODO: Development only
-const store = createStore(
-  playgroundApp,
-  applyMiddleware(thunk, logger)
-);
+const middlewares = applyMiddleware(thunk, logger);
+const enhancers = compose(middlewares,  persistState(undefined, { serialize, deserialize}));
+const store = createStore(playgroundApp, enhancers);
 
 ReactDOM.render(
   <Provider store={store}>
