024af55133fbf3c8dced3f22ba7dd8b0e1829332
Jake Goulding <jake.goulding@integer32.com>
2016-06-23 00:23:36 -0400

    Hide warnings from disabled features

--- ui/Editor.jsx
+++ ui/Editor.jsx
@@ -5,6 +5,8 @@ import brace from 'brace';
 import 'brace/mode/rust';
 import 'brace/theme/github';
 import 'brace/keybinding/emacs';
+// https://github.com/securingsincity/react-ace/issues/95
+import 'brace/ext/language_tools';
 
 export default class Editor extends React.Component {
   render() {
