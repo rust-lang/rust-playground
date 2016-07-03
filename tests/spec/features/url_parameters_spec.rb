41703260c04ec91b489519c91056f3b97790ea4d
Jake Goulding <jake.goulding@integer32.com>
2016-07-02 21:39:36 -0400

    Test that loading a gist keeps the gist links around

--- tests/spec/features/url_parameters_spec.rb
+++ tests/spec/features/url_parameters_spec.rb
@@ -8,6 +8,13 @@ RSpec.feature "Configuration by URL parameters", type: :feature, js: true do
     expect(editor).to have_line 'This source code came from a Gist'
   end
 
+  scenario "loading from a Gist preserves the links" do
+    visit '/?gist=20fb1e0475f890d0fdb7864e3ad0820c'
+
+    within('.output') { click_on 'Gist' }
+    expect(page).to have_link("Permalink to the playground")
+  end
+
   def editor
     Editor.new(page)
   end
