fc174b9048d2e4c5d27c4dfec4ea9fb43fcc34dd
Jake Goulding <jake.goulding@integer32.com>
2016-07-18 19:47:30 -0400

    Add a click action to the error line numbers

--- ui/frontend/index.js
+++ ui/frontend/index.js
@@ -9,32 +9,11 @@ import thunk from 'redux-thunk';
 import persistState from 'redux-localstorage';
 import url from 'url';
 
+import { configureRustErrors } from './highlighting';
 import { serialize, deserialize } from './local_storage';
 import playgroundApp from './reducers';
 import { performGistLoad } from './actions';
 import Playground from './Playground.jsx';
-
-import Prism from "prismjs";
-
-Prism.languages.rust_errors = {
-  'warning':/warning:.*\n/,
-  'error': {
-    pattern: /error:.*\n/,
-    inside: {
-      'error-explanation': /\[--explain E\d+\]/,
-    },
-  },
-  'error-location': /-->.*\n/,
-};
-
-Prism.hooks.add('wrap', env => {
-  if (env.type === 'error-explanation') {
-    const errorMatch = /E\d+/.exec(env.content);
-    const [errorCode] = errorMatch;
-    env.tag = 'a';
-    env.attributes.href = `https://doc.rust-lang.org/error-index.html#${errorCode}`;
-  }
-});
 
 var mw = [thunk];
 if (process.env.NODE_ENV !== 'production') {
@@ -43,6 +22,8 @@ if (process.env.NODE_ENV !== 'production') {
 const middlewares = applyMiddleware(...mw);
 const enhancers = compose(middlewares, persistState(undefined, { serialize, deserialize }));
 const store = createStore(playgroundApp, enhancers);
+
+configureRustErrors((line, col) => console.log(line, col));
 
 // Process query parameters
 const urlObj = url.parse(window.location.href, true);
