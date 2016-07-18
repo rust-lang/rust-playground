22dcae2e78e8215f7b451beef6ab288734450843
Jake Goulding <jake.goulding@integer32.com>
2016-07-17 22:40:02 -0400

    Break out subcomponents for the editor

--- ui/frontend/Editor.jsx
+++ ui/frontend/Editor.jsx
@@ -8,42 +8,45 @@ import 'brace/keybinding/emacs';
 // https://github.com/securingsincity/react-ace/issues/95
 import 'brace/ext/language_tools';
 
+function SimpleEditor(props) {
+  const { code, onEditCode } = props;
+
+  return (
+    <textarea className="editor-simple"
+              name="editor-simple"
+              value={ code }
+              onChange={ e => onEditCode(e.target.value) } />
+  );
+}
+
+function AdvancedEditor(props) {
+  const { code, onEditCode } = props;
+
+  return (
+    <AceEditor
+      mode="rust"
+      theme="github"
+      keyboardHandler="emacs"
+      value={ code }
+      onChange={ onEditCode }
+      name="editor"
+      width="100%"
+      height="100%"
+      editorProps={ { $blockScrolling: true } } />
+  );
+}
+
 export default class Editor extends React.Component {
   render() {
-    const { editor } = this.props;
+    const { editor, code, onEditCode } = this.props;
+
+    const simple = <SimpleEditor code={code} onEditCode={onEditCode} />;
+    const advanced = <AdvancedEditor code={code} onEditCode={onEditCode} />;
 
     return (
       <div className="editor">
-        { editor === "simple" ? this.simpleEditor() : this.advancedEditor() }
+        { editor === "simple" ? simple : advanced }
       </div>
-    );
-  }
-
-  simpleEditor() {
-    const { code, onEditCode } = this.props;
-
-    return (
-      <textarea className="editor-simple"
-                name="editor-simple"
-                value={ code }
-                onChange={ e => onEditCode(e.target.value) } />
-    );
-  }
-
-  advancedEditor() {
-    const { code, onEditCode } = this.props;
-
-    return (
-      <AceEditor
-         mode="rust"
-         theme="github"
-         keyboardHandler="emacs"
-         value={ code }
-         onChange={ onEditCode }
-         name="editor"
-         width="100%"
-         height="100%"
-         editorProps={ { $blockScrolling: true } } />
     );
   }
 };
