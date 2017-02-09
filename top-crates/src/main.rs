6b639e086f97fea05d359f23d45f09261f33b01d
Jake Goulding <jake.goulding@integer32.com>
2017-02-08 19:54:38 -0500

    Make Crate into a real type and directly deserialize JSON

--- top-crates/src/main.rs
+++ top-crates/src/main.rs
@@ -2,6 +2,8 @@ extern crate hyper;
 extern crate hyper_native_tls;
 extern crate serde;
 extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 extern crate toml;
 extern crate semver;
 extern crate mktemp;
@@ -20,31 +22,44 @@ use hyper_native_tls::NativeTlsClient;
 use mktemp::Temp;
 use semver::Version;
 
-type Crate = (String, String);
+#[derive(Debug, Deserialize)]
+struct TopCrates {
+    crates: Vec<Crate>,
+}
 
-fn get_top_crates() -> serde_json::Value {
+#[derive(Debug, Deserialize)]
+struct Crate {
+    #[serde(rename="id")]
+    name: String,
+    #[serde(rename="max_version")]
+    version: String,
+}
+
+impl Crate {
+    fn new<N, V>(name: N, version: V) -> Self
+        where N: Into<String>,
+              V: Into<String>,
+    {
+        Crate {
+            name: name.into(),
+            version: version.into(),
+        }
+    }
+}
+
+fn get_top_crates() -> Vec<Crate> {
     let ssl = NativeTlsClient::new().expect("Unable to build TLS client");
     let connector = HttpsConnector::new(ssl);
     let client = Client::with_connector(connector);
 
-    let res = client.get("https://crates.io/api/v1/crates?page=1&per_page=100&sort=downloads").send().expect("Could not fetch top crates");
+    let res = client
+        .get("https://crates.io/api/v1/crates?page=1&per_page=100&sort=downloads")
+        .send()
+        .expect("Could not fetch top crates");
     assert_eq!(res.status, hyper::Ok);
 
-    serde_json::from_reader(res).expect("Invalid JSON")
-}
-
-fn top_crates_to_crates(summary: serde_json::Value) -> Vec<Crate> {
-    let summary = summary.as_object().expect("Not object");
-    let most_downloaded = summary.get("crates").expect("Missing most_downloaded");
-    let most_downloaded = most_downloaded.as_array().expect("crates not an array");
-
-    most_downloaded.iter().map(|krate| {
-        let krate = krate.as_object().expect("crate not an object");
-        let id = krate.get("id").expect("missing id").as_str().expect("id not string");
-        let version = krate.get("max_version").expect("missing version").as_str().expect("version not string");
-
-        (id.into(), version.into())
-    }).collect()
+    let top: TopCrates = serde_json::from_reader(res).expect("Invalid JSON");
+    top.crates
 }
 
 fn crates_to_toml(crates: Vec<Crate>) -> toml::Value {
@@ -56,8 +71,8 @@ fn crates_to_toml(crates: Vec<Crate>) -> toml::Value {
     package.insert("version".into(), Value::String("0.0.1".into()));
 
     let mut dependencies = toml::Table::new();
-    for (id, version) in crates {
-        dependencies.insert(id, Value::String(version));
+    for Crate { name, version } in crates {
+        dependencies.insert(name, Value::String(version));
     }
 
     let mut result = toml::Table::new();
@@ -116,13 +131,13 @@ fn lockfile_to_crates(lockfile: toml::Value) -> Vec<Crate> {
         let name = package.get("name").expect("missing name").as_str().expect("name not string");
         let version = package.get("version").expect("missing version").as_str().expect("version not string");
 
-        (name.into(), version.into())
+        Crate::new(name, version)
     }).collect()
 }
 
 fn unique_latest_crates(crates: Vec<Crate>) -> Vec<Crate> {
     let mut uniqs = HashMap::new();
-    for (name, version) in crates {
+    for Crate { name, version } in crates {
         let version = Version::parse(&version).expect("Invalid version");
 
         match uniqs.entry(name) {
@@ -137,7 +152,7 @@ fn unique_latest_crates(crates: Vec<Crate>) -> Vec<Crate> {
         }
     }
 
-    uniqs.into_iter().map(|(n, v)| (n, v.to_string())).collect()
+    uniqs.into_iter().map(|(n, v)| Crate::new(n, v.to_string())).collect()
 }
 
 static BLACKLIST: &'static [&'static str] = &[
@@ -156,14 +171,15 @@ static BLACKLIST: &'static [&'static str] = &[
 ];
 
 fn remove_blacklisted_crates(crates: Vec<Crate>) -> Vec<Crate> {
-    crates.into_iter().filter(|&(ref name, _)| !BLACKLIST.contains(&&**name)).collect()
+    crates.into_iter()
+        .filter(|&Crate { ref name, .. }| !BLACKLIST.contains(&name.as_str()))
+        .collect()
 }
 
 fn main() {
     let scratch = Temp::new_dir().expect("unable to make scratch dir");
 
-    let top_crates = get_top_crates();
-    let crates = top_crates_to_crates(top_crates);
+    let crates = get_top_crates();
     let crates = remove_blacklisted_crates(crates);
 
     let cargo_toml_rev1 = crates_to_toml(crates);
