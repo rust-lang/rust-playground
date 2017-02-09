5f789bc4fc4be91152ba013257e455c4e70af0b3
David Tolnay <dtolnay@gmail.com>
2017-02-09 00:50:19 -0800

    Rewrite crate resolution to use Cargo directly

--- top-crates/src/main.rs
+++ top-crates/src/main.rs
@@ -1,26 +1,23 @@
+extern crate cargo;
 extern crate hyper;
 extern crate hyper_native_tls;
-extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 extern crate toml;
-extern crate semver;
-extern crate mktemp;
 
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
-use std::fs::{self, File};
-use std::io::prelude::*;
-use std::io::{BufReader, BufWriter};
-use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::collections::BTreeMap;
+use std::collections::btree_map::Entry;
+use std::fs::File;
+use std::io::Write;
 
+use cargo::core::{Dependency, Registry, Source, SourceId};
+use cargo::core::resolver::{self, Method, Resolve};
+use cargo::sources::RegistrySource;
+use cargo::util::Config;
 use hyper::client::Client;
 use hyper::net::HttpsConnector;
 use hyper_native_tls::NativeTlsClient;
-use mktemp::Temp;
-use semver::Version;
 
 #[derive(Debug, Deserialize)]
 struct TopCrates {
@@ -35,19 +32,22 @@ struct Crate {
     version: String,
 }
 
-impl Crate {
-    fn new<N, V>(name: N, version: V) -> Self
-        where N: Into<String>,
-              V: Into<String>,
-    {
-        Crate {
-            name: name.into(),
-            version: version.into(),
-        }
-    }
+/// A Cargo.toml file.
+#[derive(Serialize)]
+struct TomlManifest {
+    package: Package,
+    dependencies: BTreeMap<String, String>,
 }
 
-fn get_top_crates() -> Vec<Crate> {
+/// Header of Cargo.toml file.
+#[derive(Serialize)]
+struct Package {
+    name: String,
+    version: String,
+    authors: Vec<String>,
+}
+
+fn get_top_crates() -> TopCrates {
     let ssl = NativeTlsClient::new().expect("Unable to build TLS client");
     let connector = HttpsConnector::new(ssl);
     let client = Client::with_connector(connector);
@@ -58,101 +58,48 @@ fn get_top_crates() -> Vec<Crate> {
         .expect("Could not fetch top crates");
     assert_eq!(res.status, hyper::Ok);
 
-    let top: TopCrates = serde_json::from_reader(res).expect("Invalid JSON");
-    top.crates
+    serde_json::from_reader(res).expect("Invalid JSON")
 }
 
-fn crates_to_toml(crates: Vec<Crate>) -> toml::Value {
-    use toml::value::{Value, Table};
-
-    let mut package = Table::new();
-    package.insert("authors".into(), Value::Array(vec![Value::String("The Rust Playground".into())]));
-    package.insert("name".into(), Value::String("playground".into()));
-    package.insert("version".into(), Value::String("0.0.1".into()));
-
-    let mut dependencies = Table::new();
-    for Crate { name, version } in crates {
-        dependencies.insert(name, Value::String(version));
+fn decide_features() -> Method<'static> {
+    Method::Required {
+        dev_deps: false,
+        features: &[],
+        uses_default_features: true,
     }
-
-    let mut result = Table::new();
-    result.insert("package".into(), Value::Table(package));
-    result.insert("dependencies".into(), Value::Table(dependencies));
-
-    Value::Table(result)
 }
 
-fn write_cargo_toml(dir: &Path, cargo_toml: toml::Value) -> PathBuf {
-    let toml_file = dir.join("Cargo.toml");
+fn unique_latest_crates(resolve: Resolve) -> BTreeMap<String, String> {
+    let mut uniqs = BTreeMap::new();
+    for pkg in resolve.iter() {
+        // Skip blacklisted crates.
+        if BLACKLIST.contains(&pkg.name()) {
+            continue;
+        }
 
-    let f = File::create(&toml_file).expect("Unable to create Cargo.toml");
-    let mut f = BufWriter::new(f);
-    let data = toml::to_vec(&cargo_toml).expect("Unable to encode TOML");
-    f.write_all(&data).expect("Couldn't write Cargo.toml");
-
-    toml_file
-}
-
-fn resolve_dependencies(dir: &Path, cargo_toml: toml::Value) -> PathBuf {
-    let x = Command::new("cargo")
-        .args(&["new", "--bin", "dependencies"])
-        .current_dir(dir)
-        .status().expect("Couldn't create scratch project");
-    assert!(x.success(), "Didn't run cargo new");
-
-    let project_dir = dir.join("dependencies");
-    write_cargo_toml(&project_dir, cargo_toml);
-
-    let x = Command::new("cargo")
-        .args(&["fetch"])
-        .current_dir(&project_dir)
-        .status().expect("Couldn't resolve dependencies");
-    assert!(x.success(), "Didn't run cargo fetch");
-
-    project_dir.join("Cargo.lock")
-}
-
-fn get_lockfile(lockfile_path: &Path) -> toml::Value {
-    let f = File::open(lockfile_path).expect("Couldn't open the lockfile");
-    let mut f = BufReader::new(f);
-
-    let mut s = String::new();
-    f.read_to_string(&mut s).expect("Couldn't read the lockfile");
-
-    toml::from_str(&s).expect("Unable to parse lockfile")
-}
-
-fn lockfile_to_crates(lockfile: toml::Value) -> Vec<Crate> {
-    let packages = lockfile.get("package").expect("Couldn't find packages");
-    let packages = packages.as_array().expect("packages not an array");
-
-    packages.iter().map(|package| {
-        let package = package.as_table().expect("not an object");
-        let name = package.get("name").expect("missing name").as_str().expect("name not string");
-        let version = package.get("version").expect("missing version").as_str().expect("version not string");
-
-        Crate::new(name, version)
-    }).collect()
-}
-
-fn unique_latest_crates(crates: Vec<Crate>) -> Vec<Crate> {
-    let mut uniqs = HashMap::new();
-    for Crate { name, version } in crates {
-        let version = Version::parse(&version).expect("Invalid version");
-
-        match uniqs.entry(name) {
+        match uniqs.entry(pkg.name()) {
             Entry::Vacant(entry) => {
-                entry.insert(version);
+                // First time seeing this package.
+                entry.insert(pkg.version());
             }
             Entry::Occupied(mut entry) => {
-                if &version > entry.get() {
-                    entry.insert(version);
+                // Seen before, keep the newest version.
+                if &pkg.version() > entry.get() {
+                    entry.insert(pkg.version());
                 }
             }
         }
     }
 
-    uniqs.into_iter().map(|(n, v)| Crate::new(n, v.to_string())).collect()
+    uniqs.into_iter()
+         .map(|(name, version)| (name.to_string(), version.to_string()))
+         .collect()
+}
+
+fn write_manifest(manifest: TomlManifest, path: &str) {
+    let mut f = File::create(path).expect("Unable to create Cargo.toml");
+    let content = toml::to_vec(&manifest).expect("Couldn't serialize TOML");
+    f.write_all(&content).expect("Couldn't write Cargo.toml");
 }
 
 static BLACKLIST: &'static [&'static str] = &[
@@ -170,28 +117,56 @@ static BLACKLIST: &'static [&'static str] = &[
     "redox_syscall", // Not supported on stable
 ];
 
-fn remove_blacklisted_crates(crates: Vec<Crate>) -> Vec<Crate> {
-    crates.into_iter()
-        .filter(|&Crate { ref name, .. }| !BLACKLIST.contains(&name.as_str()))
-        .collect()
-}
-
 fn main() {
-    let scratch = Temp::new_dir().expect("unable to make scratch dir");
+    // Setup to interact with cargo.
+    let config = Config::default().expect("Unable to create default Cargo config");
+    let crates_io = SourceId::crates_io(&config).expect("Unable to create crates.io source ID");
+    let mut registry = RegistrySource::remote(&crates_io, &config);
+    registry.update().expect("Unable to update registry");
 
-    let crates = get_top_crates();
-    let crates = remove_blacklisted_crates(crates);
+    // List top 100 crates by number of downloads on crates.io.
+    let top = get_top_crates();
 
-    let cargo_toml_rev1 = crates_to_toml(crates);
-    let lockfile_path = resolve_dependencies(scratch.as_ref(), cargo_toml_rev1);
+    let mut summaries = Vec::new();
+    for Crate { ref name, ref version } in top.crates {
+        // Skip blacklisted crates.
+        if BLACKLIST.contains(&&name[..]) {
+            continue;
+        }
 
-    let lockfile = get_lockfile(&lockfile_path);
-    let crates_rev2 = lockfile_to_crates(lockfile);
+        // Query the registry for summary of this crate.
+        let dep = Dependency::parse_no_deprecated(name, Some(version), &crates_io)
+            .unwrap_or_else(|e| panic!("Unable to parse dependency for {}:{}: {}", name, version, e));
 
-    let crates_rev2 = unique_latest_crates(crates_rev2);
-    let crates_rev2 = remove_blacklisted_crates(crates_rev2);
-    let cargo_toml_rev2 = crates_to_toml(crates_rev2);
+        let matches = registry.query(&dep).unwrap_or_else(|e| {
+            panic!("Unable to query registry for {}:{}: {}", name, version, e);
+        });
+        if matches.len() != 1 {
+            panic!("expected one registry match for `{}:{}`", name, version);
+        }
+        let summary = matches.into_iter().next().unwrap();
 
-    let result_file = write_cargo_toml(scratch.as_ref(), cargo_toml_rev2);
-    fs::rename(result_file, "result.Cargo.toml").expect("Couldn't rename");
+        // Add a dependency on this crate.
+        let method = decide_features();
+        summaries.push((summary, method));
+    }
+
+    // Resolve transitive dependencies.
+    let res = resolver::resolve(&summaries, &[], &mut registry)
+        .expect("Unable to resolve dependencies");
+
+    // Construct playground's Cargo.toml.
+    let manifest = TomlManifest {
+        package: Package {
+            name: "playground".to_owned(),
+            version: "0.0.1".to_owned(),
+            authors: vec!["The Rust Playground".to_owned()],
+        },
+        dependencies: unique_latest_crates(res),
+    };
+
+    // Write manifest file.
+    let cargo_toml = "result.Cargo.toml";
+    write_manifest(manifest, cargo_toml);
+    println!("wrote {}", cargo_toml);
 }
