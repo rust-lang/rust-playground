53ae19b4395d6d45fe84554a03f97a6c6a434de8
Jake Goulding <jake.goulding@integer32.com>
2017-02-16 12:28:16 -0500

    Save failure screenshots in a directory

--- tests/spec/spec_helper.rb
+++ tests/spec/spec_helper.rb
@@ -38,6 +38,7 @@ Capybara.javascript_driver = :poltergeist
 Capybara.app_host = "http://#{ADDRESS}:#{PORT}"
 Capybara.run_server = false
 Capybara.default_max_wait_time = 5
+Capybara.save_path = File.join(Dir.pwd, "screenshots")
 
 RSpec.configure do |config|
   config.before do
