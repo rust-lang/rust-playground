273710bb9271b123374ea43d4183983bf82cca05
Jake Goulding <jake.goulding@integer32.com>
2017-03-21 21:27:26 -0400

    Add tests for navigating around the pages

---
+++ tests/spec/features/navigation_spec.rb
@@ -0,0 +1,92 @@
+require 'spec_helper'
+require 'support/editor'
+require 'support/playground_actions'
+
+RSpec.feature "Navigating between pages", type: :feature, js: true do
+  include PlaygroundActions
+
+  RSpec::Matchers.define :be_at_url do |path, query = {}|
+    match do |page|
+      uri = URI::parse(page.current_url)
+      expect(uri.path).to eql(path)
+
+      query = query.map { |k, v| [k.to_s, Array(v).map(&:to_s)] }.to_h
+      query_hash = CGI::parse(uri.query || '')
+      expect(query_hash).to include(query)
+    end
+
+    failure_message do |page|
+      "expected that #{page.current_url} would be #{path} with the query parameters #{query}"
+    end
+  end
+
+  # This is kind of a test of the router library too, so if that ever
+  # gets extracted, a chunk of these tests can be removed.
+
+  scenario "Mode and channel buttons update the URL" do
+    visit '/'
+    expect(page).to have_content('Run')
+
+    editor.set("dummy")
+    expect(page).to be_at_url('/')
+
+    choose_styled('Release')
+    expect(page).to be_at_url('/', version: 'stable', mode: 'release')
+
+    go_back
+    expect(page).to be_at_url('/')
+
+    choose_styled('Release')
+    choose_styled('Beta')
+    expect(page).to be_at_url('/', version: 'beta', mode: 'release')
+
+    go_back
+    go_back
+    expect(page).to be_at_url('/')
+  end
+
+  scenario "Navigating to help changes the URL" do
+    visit '/'
+    expect(page).to have_content('Run')
+
+    click_on '?'
+    expect(page).to be_at_url('/help')
+    expect(page).to have_content('The Alternative Rust Playground')
+
+    go_back
+    expect(page).to be_at_url('/')
+    expect(page).to have_content('Run')
+
+    go_forward
+    expect(page).to be_at_url('/help')
+    expect(page).to have_content('The Alternative Rust Playground')
+
+    click_on "Return to the playground"
+    expect(page).to be_at_url('/')
+    expect(page).to have_content('Run')
+  end
+
+  scenario "Navigating from help changes the URL" do
+    visit '/help'
+    expect(page).to have_content('The Alternative Rust Playground')
+
+    click_on "Return to the playground"
+    expect(page).to be_at_url('/')
+    expect(page).to have_content('Run')
+  end
+
+  def editor
+    Editor.new(page)
+  end
+
+  # It probably would be worth figuring out why
+  # `page.go_{back,forward}` doesn't work... :-(
+  # Maybe https://github.com/teampoltergeist/poltergeist/issues/624
+  def go_back
+    page.execute_script('window.history.go(-1);')
+  end
+
+  def go_forward
+    page.execute_script('window.history.go(1);')
+  end
+end
