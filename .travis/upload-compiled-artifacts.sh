0df9dd728a0a15dc60a24f662e3f2da667f0a1be
Jake Goulding <jake.goulding@integer32.com>
2017-05-15 21:09:05 -0400

    Upload compiled artifacts from the master branch

---
+++ .travis/upload-compiled-artifacts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euv -o pipefail
+
+if [[ ("${TRAVIS_PULL_REQUEST}" == "false") &&
+          ("${TRAVIS_BRANCH}" == "master") &&
+          (-n "${AWS_ACCESS_KEY_ID}") &&
+          (-n "${AWS_SECRET_ACCESS_KEY}") ]]
+then
+    if [[ $BUILDING == backend ]]; then
+        aws s3 sync $HOME/cache/rust/target/x86_64-unknown-linux-musl/release/ui s3://playground-artifacts
+    else
+        aws s3 sync $TRAVIS_BUILD_DIR/ui/frontend/build/ s3://playground-artifacts/build
+    fi
+fi
