5cb847208c359cbef584a7b7adefd104a517c044
Jake Goulding <jake.goulding@integer32.com>
2017-08-26 17:04:43 -0400

    PageSwitcher

--- ui/frontend/PageSwitcher.tsx
+++ ui/frontend/PageSwitcher.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Playground from './Playground';
 import Help from './Help';
 
-const PageSwitcher = ({ page }) => (
+const PageSwitcher: React.SFC<Props> = ({ page }) => (
   page === 'index' ? <Playground /> : <Help />
 );
 
-PageSwitcher.propTypes = {
-  page: PropTypes.string.isRequired,
+interface Props {
+  page: string,
 };
 
 const mapStateToProps = ({ page }) => ({ page });
