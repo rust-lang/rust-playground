beea75a94bce22d77db8187ce3ce6e80db7c10df
Jake Goulding <jake.goulding@integer32.com>
2017-10-25 09:37:11 -0400

    Upgrade React-Ace
    No longer need to trigger an ACE resize manually

--- ui/frontend/Playground.tsx
+++ ui/frontend/Playground.tsx
@@ -16,49 +16,31 @@ const ConfigurationModal: React.SFC = () => (
   </div>
 );
 
-class Playground extends React.Component<Props> {
-  public render() {
-    const { showConfig, focus, splitOrientation } = this.props;
+const Playground: React.SFC<Props> = ({ showConfig, focus, splitOrientation }) => {
+  const config = showConfig ? <ConfigurationModal /> : null;
+  const outputFocused = focus ? 'playground-output-focused' : '';
+  const splitClass = 'playground-split';
+  const orientation = splitClass + '-' + splitOrientation;
 
-    const config = showConfig ? <ConfigurationModal /> : null;
-    const outputFocused = focus ? 'playground-output-focused' : '';
-    const splitClass = 'playground-split';
-    const orientation = splitClass + '-' + splitOrientation;
-
-    return (
-      <div>
-        {config}
-        <div className="playground">
-          <div className="playground-header">
-            <Header />
+  return (
+    <div>
+      {config}
+      <div className="playground">
+        <div className="playground-header">
+          <Header />
+        </div>
+        <div className={`${splitClass} ${orientation}`}>
+          <div className="playground-editor">
+            <Editor />
           </div>
-          <div className={`${splitClass} ${orientation}`}>
-            <div className="playground-editor">
-              <Editor />
-            </div>
-            <div className={`playground-output ${outputFocused}`}>
-              <Output />
-            </div>
+          <div className={`playground-output ${outputFocused}`}>
+            <Output />
           </div>
         </div>
       </div>
-    );
-  }
-
-  public componentDidUpdate(prevProps, _prevState) {
-    if (this.props.focus !== prevProps.focus) {
-      // Inform the ACE editor that its size has changed.
-      try {
-        window.dispatchEvent(new Event('resize'));
-      } catch (ex) {
-        // IE 11
-        const evt = window.document.createEvent('UIEvents');
-        evt.initUIEvent('resize', true, false, window, 0);
-        window.dispatchEvent(evt);
-      }
-    }
-  }
-}
+    </div>
+  );
+};
 
 interface Props {
   focus?: string;
@@ -68,9 +50,9 @@ interface Props {
 
 const mapStateToProps = ({
     configuration: { shown: showConfig, orientation: splitOrientation },
-    output: { meta: { focus } },
+  output: { meta: { focus } },
 }: State) => (
-  { showConfig, focus, splitOrientation }
-);
+    { showConfig, focus, splitOrientation }
+  );
 
 export default connect(mapStateToProps, undefined)(Playground);
