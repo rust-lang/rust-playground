41a200ca40d20214864e1101549c5a66bafabde9
Jake Goulding <jake.goulding@integer32.com>
2017-11-26 11:57:52 -0500

    Add WebAssembly support to the docker containers
    We add a custom `cargo wasm` command that behaves similar enough to
    the existing `cargo rustc` to be used without changing too much
    infrastructure. For right now, this is only supported in the nightly channel.
    
    We also remove all crates instead of trying to figure out which ones
    are valid. This leaves an ugly UI gap as we don't have a way of
    supporting two distinct sets of crates at the moment.

---
+++ compiler/base/postinstall.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function install_wasm_target() {
+    rustup target add wasm32-unknown-unknown
+}
+
+function install_wasm2wat() {
+    cd /tmp
+    git clone https://github.com/WebAssembly/wabt
+    cd /tmp/wabt/
+    cmake -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
+    make wasm2wat
+    cp ./wasm2wat $HOME/.cargo/bin
+    rm -rf /tmp/wabt/
+}
+
+function install_wasm_gc() {
+    cargo install --git https://github.com/alexcrichton/wasm-gc
+}
+
+if [[ $1 == "nightly" ]]; then
+    (install_wasm_target)
+    (install_wasm2wat)
+    (install_wasm_gc)
+fi
