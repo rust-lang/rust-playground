8dcd645ddec6244981f418e500db427d7f60200e
Jake Goulding <jake.goulding@integer32.com>
2018-01-01 13:28:45 -0500

    Move artifacts to a new bucket
    Previously, I had this hosted on my personal AWS account; let's move
    it to one owned by Integer32.

--- .travis/upload-compiled-artifacts.sh
+++ .travis/upload-compiled-artifacts.sh
@@ -8,8 +8,8 @@ if [[ ("${TRAVIS_PULL_REQUEST}" == "false") &&
           (-n "${AWS_SECRET_ACCESS_KEY}") ]]
 then
     if [[ $BUILDING == backend ]]; then
-        aws s3 cp $HOME/cache/rust/target/x86_64-unknown-linux-musl/release/ui s3://playground-artifacts
+        aws s3 cp --region=us-east-2 $HOME/cache/rust/target/x86_64-unknown-linux-musl/release/ui s3://playground-artifacts-i32
     else
-        aws s3 sync $TRAVIS_BUILD_DIR/ui/frontend/build/ s3://playground-artifacts/build
+        aws s3 sync --region=us-east-2 $TRAVIS_BUILD_DIR/ui/frontend/build/ s3://playground-artifacts-i32/build
     fi
 fi
--- deployment/update.sh
+++ deployment/update.sh
@@ -11,16 +11,17 @@ $root/rust-playground/compiler/fetch.sh
 docker system prune -f || true
 
 # Get new artifacts
-aws s3 sync s3://playground-artifacts $root/playground-artifacts
+aws s3 sync --region=us-east-2 s3://playground-artifacts-i32 $root/playground-artifacts
 # These artifacts don't change names and might stay the same size
 # https://github.com/aws/aws-cli/issues/1074
 aws s3 sync \
+    --region=us-east-2 \
     --exclude='*' \
     --include=ui \
     --include=build/index.html \
     --include=build/robots.txt \
     --exact-timestamps \
-    s3://playground-artifacts $root/playground-artifacts
+    s3://playground-artifacts-i32 $root/playground-artifacts
 chmod +x $root/playground-artifacts/ui
 
 # Restart to get new server binary
