0d18b6aecb7c480d6a41213e857dd62416af4a2d
Jake Goulding <jake.goulding@integer32.com>
2018-07-23 23:11:37 -0400

    Restore the backtrace tests

--- tests/spec/features/backtrace_spec.rb
+++ tests/spec/features/backtrace_spec.rb
@@ -1,26 +1,45 @@
 require 'spec_helper'
 require 'support/editor'
+require 'support/playground_actions'
 
 RSpec.feature "A backtrace is shown for certain errors", type: :feature, js: true do
-  before { pending "Backtracing has a large performance penalty" }
+  include PlaygroundActions
 
   before do
     visit '/'
     editor.set(code)
-    within('.header') { click_on("Run") }
   end
 
-  scenario "a stack trace is shown" do
-    within('.output-stderr') do
-      expect(page).to have_content 'stack backtrace:'
-      expect(page).to have_content 'rust_begin_unwind'
+  context "backtraces are enabled" do
+    before do
+      in_advanced_options_menu { choose 'enabled' }
+      within('.header') { click_on("Run") }
+    end
+
+    scenario "a backtrace is shown" do
+      within('.output-stderr') do
+        expect(page).to have_content 'stack backtrace:'
+        expect(page).to have_content 'rust_begin_unwind'
+      end
+    end
+
+    scenario "filenames link to that line of code" do
+      within('.output-stderr') do
+        expect(page).to have_link('main.rs:2')
+        expect(page).to have_link('main.rs:6')
+      end
     end
   end
 
-  scenario "filenames link to that line of code" do
-    within('.output-stderr') do
-      expect(page).to have_link('main.rs:2')
-      expect(page).to have_link('main.rs:6')
+  context "backtraces are disabled" do
+    before do
+      within('.header') { click_on("Run") }
+    end
+
+    scenario "the backtrace suggestion is a link" do
+      within('.output-stderr') do
+        expect(page).to have_link(text: /Run with .* a backtrace/)
+      end
     end
   end
 
