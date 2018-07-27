1de38f5d4ae3833cb3c960df9cec322426895632
Jake Goulding <jake.goulding@integer32.com>
2018-07-26 22:32:19 -0400

    Move Clippy to be a rustup component and remove crates.io version checks
    Closes #246

--- compiler/build.sh
+++ compiler/build.sh
@@ -33,22 +33,12 @@ crate_api_base=https://crates.io/api/v1/crates
 for tool in $tools_to_build; do
     cd "${tool}"
 
-    crate_name="${tool}"
-    if [[ "${tool}" == 'rustfmt' ]]; then
-        crate_name=rustfmt-nightly
-    fi
-
-    filename="version-${tool}.txt"
-    curl -o "${filename}" "${crate_api_base}/${crate_name}"
-    version=$(jq -r '.crate.max_version' "${filename}")
-
     image_name="${tool}"
     full_name="${repository}/${image_name}"
 
     docker pull "${full_name}"
     docker build -t "${full_name}" \
            --cache-from "${full_name}" \
-           --build-arg version="${version}" \
            .
     docker tag "${full_name}" "${image_name}"
 
