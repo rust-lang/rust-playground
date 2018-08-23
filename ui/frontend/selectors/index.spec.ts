aab5efde9e74a03b24001b9d454ee054a2c32c82
Jake Goulding <jake.goulding@integer32.com>
2018-08-23 09:23:55 -0400

    Introduce JS tests

---
+++ ui/frontend/selectors/index.spec.ts
@@ -0,0 +1,40 @@
+import reducer from '../reducers';
+
+import { hasMainFunctionSelector } from './index';
+
+const buildState = code => {
+  const state = reducer(undefined, { type: '__test_init' });
+  state.code = code;
+  return state;
+};
+
+describe('checking for a main function', () => {
+  test('empty code has no main', () => {
+    expect(hasMainFunctionSelector(buildState(''))).toBe(false);
+  });
+
+  test('a plain main counts', () => {
+    expect(hasMainFunctionSelector(buildState('fn main()'))).toBe(true);
+  });
+
+  test('a public main counts', () => {
+    expect(hasMainFunctionSelector(buildState('pub fn main()'))).toBe(true);
+  });
+
+  test('leading indentation is ignored', () => {
+    expect(hasMainFunctionSelector(buildState('\t fn main()'))).toBe(true);
+  });
+
+  test('extra space everywhere is ignored', () => {
+    expect(hasMainFunctionSelector(buildState('  pub  fn  main  (  )'))).toBe(true);
+  });
+
+  test('a commented-out main does not count', () => {
+    expect(hasMainFunctionSelector(buildState('// fn main()'))).toBe(false);
+    expect(hasMainFunctionSelector(buildState('/* fn main()'))).toBe(false);
+  });
+
+  test('a function with the substring main does not count', () => {
+    expect(hasMainFunctionSelector(buildState('fn mainly()'))).toBe(false);
+  });
+});
