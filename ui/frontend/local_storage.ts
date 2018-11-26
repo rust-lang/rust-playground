b86750481f4c5dbf0d33d5dbc4373fce2b91b405
Jake Goulding <jake.goulding@integer32.com>
2018-11-26 08:09:54 -0500

    Handle when a null value has been serialized

--- ui/frontend/local_storage.ts
+++ ui/frontend/local_storage.ts
@@ -29,6 +29,7 @@ export function serialize(state: State) {
 export function deserialize(savedState) {
   if (!savedState) { return undefined; }
   const parsedState = JSON.parse(savedState);
+  if (!parsedState) { return undefined; }
   if (parsedState.version !== CURRENT_VERSION) { return undefined; }
 
   // This assumes that the keys we serialize with match the keys in the
