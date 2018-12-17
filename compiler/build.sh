827fc369abf7d3ba9d44281d9099b6253b5e6e83
CjS77 <cayle.sharrock@gmail.com>
2018-12-17 19:52:52 +0200

    Remove stable because dalek won't compile

--- compiler/build.sh
+++ compiler/build.sh
@@ -2,7 +2,7 @@
 
 set -euv -o pipefail
 
-channels_to_build="${CHANNELS_TO_BUILD-stable beta nightly}"
+channels_to_build="${CHANNELS_TO_BUILD-beta nightly}"
 tools_to_build="${TOOLS_TO_BUILD-rustfmt clippy}"
 perform_push="${PERFORM_PUSH-false}"
 
