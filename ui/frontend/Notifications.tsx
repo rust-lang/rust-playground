67bd83d0b9395b1b68083cebeca74a18035fc53b
Jake Goulding <jake.goulding@integer32.com>
2019-08-12 21:51:54 -0400

    Hook conversion: Notifications

--- ui/frontend/Notifications.tsx
+++ ui/frontend/Notifications.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Portal } from 'react-portal';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Close } from './Icon';
 
-import { seenRust2018IsDefault } from './actions';
-import { showRust2018IsDefaultSelector } from './selectors';
-import State from './state';
+import * as actions from './actions';
+import * as selectors from './selectors';
 
 const EDITION_URL = 'https://doc.rust-lang.org/edition-guide/';
 
-interface NotificationsProps {
-  seenRust2018IsDefault: () => void;
-  showRust2018IsDefault: boolean;
-}
+const Notifications: React.SFC = () => {
+  return (
+    <Portal>
+      <div className="notifications">
+        <Rust2018IsDefaultNotification />
+      </div>
+    </Portal>
+  );
+};
 
-const Notifications: React.SFC<NotificationsProps> = props => (
-  <Portal>
-    <div className="notifications">
-      {props.showRust2018IsDefault && <Rust2018IsDefaultNotification {...props} />}
-    </div>
-  </Portal>
-);
+const Rust2018IsDefaultNotification: React.SFC = () => {
+  const showRust2018IsDefault = useSelector(selectors.showRust2018IsDefaultSelector);
 
-const Rust2018IsDefaultNotification: React.SFC<NotificationsProps> = props => (
-  <Notification onClose={props.seenRust2018IsDefault}>
-    As of Rust 1.31, the default edition of Rust is now Rust
-    2018. Learn more about editions in the <a href={EDITION_URL}>Edition Guide</a>.
-    To specify which edition to use, use the advanced compilation options menu.
-  </Notification>
-);
+  const dispatch = useDispatch();
+  const seenRust2018IsDefault = useCallback(() => dispatch(actions.seenRust2018IsDefault()), [dispatch]);
+
+  return showRust2018IsDefault && (
+    <Notification onClose={seenRust2018IsDefault}>
+      As of Rust 1.31, the default edition of Rust is now Rust
+        2018. Learn more about editions in the <a href={EDITION_URL}>Edition Guide</a>.
+  To specify which edition to use, use the advanced compilation options menu.
+    </Notification>
+  );
+};
 
 interface NotificationProps {
   onClose: () => void;
@@ -42,12 +45,4 @@ const Notification: React.SFC<NotificationProps> = ({ onClose, children }) => (
   </div>
 );
 
-const mapStateToProps = (state: State) => ({
-  showRust2018IsDefault: showRust2018IsDefaultSelector(state),
-});
-
-const mapDispatchToProps = ({
-  seenRust2018IsDefault,
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Notifications);
+export default Notifications;
