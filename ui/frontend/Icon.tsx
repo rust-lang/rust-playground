8fed9c4c880fe1c7d60006660e7cdba2b467ce92
rchaser53 <tayoshizawa29@gmail.com>
2020-01-29 21:55:26 +0900

    make resizable icon

--- ui/frontend/Icon.tsx
+++ ui/frontend/Icon.tsx
@@ -36,19 +36,25 @@ export const MoreOptionsActiveIcon = () => (
   </svg>
 );
 
-// M.D. 'settings',
-export const ConfigIcon = () => (
-  <svg className="icon" height="15" viewBox="0 0 24 24" width="15" opacity="0.5" xmlns="http://www.w3.org/2000/svg">
+const createConfigIcon = (size: string) => () => (
+  <svg className="icon" height={size} viewBox="0 0 24 24" width={size} opacity="0.5" xmlns="http://www.w3.org/2000/svg">
     <path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z" />
+  </svg>
+)
+
+// M.D. 'settings',
+export const ConfigIcon = createConfigIcon('15');
+export const RustfmtTomlIcon = createConfigIcon('30');
+
+const createHelpIcon = (size: string) => () => (
+  <svg className="icon" height={size} viewBox="0 0 24 24" width={size} opacity="0.5" xmlns="http://www.w3.org/2000/svg">
+    <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z" />
   </svg>
 );
 
 // M.D. 'help_outline'
-export const HelpIcon = () => (
-  <svg className="icon" height="18" viewBox="0 0 24 24" width="18" opacity="0.5" xmlns="http://www.w3.org/2000/svg">
-    <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z" />
-  </svg>
-);
+export const HelpIcon = createHelpIcon('18');
+export const RustfmtModalHelpIcon = createHelpIcon('24');
 
 export const CheckmarkIcon = () => (
   <svg className="icon icon--checkmark" height="18" viewBox="2 2 22 22" width="18" opacity="0.5" xmlns="http://www.w3.org/2000/svg">
