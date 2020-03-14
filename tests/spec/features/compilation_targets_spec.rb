bca54b9e7b0213d3392c7d235f895e6cedef9ae7
Jake Goulding <jake.goulding@integer32.com>
2020-03-14 08:42:22 -0400

    Update test error text

--- tests/spec/features/compilation_targets_spec.rb
+++ tests/spec/features/compilation_targets_spec.rb
@@ -111,7 +111,7 @@ RSpec.feature "Compiling to different formats", type: :feature, js: true do
       in_build_menu { click_on("MIR") }
 
       within('.output-stderr') do
-        expect(page).to have_content 'an un-closed delimiter'
+        expect(page).to have_content 'an unclosed delimiter'
       end
     end
   end
