9804256cc695590c10bdb0a0918ed981a46fea2b
Jake Goulding <jake.goulding@integer32.com>
2020-05-31 13:25:55 -0400

    Ensure that clicking on the MIR source links highlights the text

--- tests/spec/support/editor.rb
+++ tests/spec/support/editor.rb
@@ -15,4 +15,17 @@ class Editor
   def has_line?(text)
     page.has_css? '.ace_line', text: text
   end
+
+  def has_highlighted_text?(text)
+    page.within('.editor .ace_text-input', visible: :any) do
+      selected = page.evaluate_script <<~JS
+        (() => {
+          const editor = document.querySelector('.ace_editor').env.editor;
+          return editor.getSelectedText();
+        })()
+      JS
+
+      selected == text
+    end
+  end
 end
