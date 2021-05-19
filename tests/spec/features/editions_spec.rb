b8b5bafed5b16f35d0c51f143ca55948479940cd
Jake Goulding <jake.goulding@integer32.com>
2021-05-18 21:01:41 -0400

    Test that selecting Rust 2021 works

--- tests/spec/features/editions_spec.rb
+++ tests/spec/features/editions_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Multiple Rust editions", type: :feature, js: true do
 
   before do
     visit '/'
-    editor.set(rust_2018_code)
+    editor.set(rust_edition_code)
   end
 
   scenario "using the 2015 edition" do
@@ -15,8 +15,7 @@ RSpec.feature "Multiple Rust editions", type: :feature, js: true do
     click_on("Run")
 
     within(:output, :stderr) do
-      expect(page).to have_content 'unused variable: `async`'
-      expect(page).to_not have_content 'expected identifier, found keyword `async`'
+      expect(page).to have_content 'cannot find struct, variant or union type `async` in this scope'
     end
   end
 
@@ -25,8 +24,16 @@ RSpec.feature "Multiple Rust editions", type: :feature, js: true do
     click_on("Run")
 
     within(:output, :stderr) do
-      expect(page).to have_content 'expected identifier, found keyword `async`'
-      expect(page).to_not have_content 'unused variable: `async`'
+      expect(page).to have_content "thread 'main' panicked at 'Box<Any>'"
+    end
+  end
+
+  scenario "using the 2021 edition" do
+    in_advanced_options_menu { select '2021' }
+    click_on("Run")
+
+    within(:output, :stderr) do
+      expect(page).to have_content 'format argument must be a string literal', wait: 10
     end
   end
 
@@ -34,10 +41,11 @@ RSpec.feature "Multiple Rust editions", type: :feature, js: true do
     Editor.new(page)
   end
 
-  def rust_2018_code
+  def rust_edition_code
     <<~EOF
+    #![allow(non_fmt_panic)]
     fn main() {
-        let async = 42;
+        panic!(async {})
     }
     EOF
   end
