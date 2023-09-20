73e8484e9f9f435ddb7c148102bed9b5be88ad29
Jake Goulding <jake.goulding@integer32.com>
2023-09-20 16:16:09 -0400

    Install wasm2wat via a Docker layer

--- compiler/base/postinstall.sh
+++ compiler/base/postinstall.sh
@@ -2,19 +2,8 @@
 
 set -eu
 
-function install_wasm2wat() {
-    cd /tmp
-    git clone https://github.com/WebAssembly/wabt
-    cd /tmp/wabt/
-    cmake -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release
-    make wasm2wat
-    cp ./wasm2wat $HOME/.cargo/bin
-    rm -rf /tmp/wabt/
-}
-
 function install_wasm_gc() {
     cargo install wasm-gc
 }
 
-(install_wasm2wat)
 (install_wasm_gc)
