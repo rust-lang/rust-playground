4315d905ac07e281f0c795db976a625809cabf60
Jake Goulding <jake.goulding@integer32.com>
2023-09-20 15:52:22 -0400

    Install Wasm target when we install Rust

--- compiler/base/postinstall.sh
+++ compiler/base/postinstall.sh
@@ -2,10 +2,6 @@
 
 set -eu
 
-function install_wasm_target() {
-    rustup target add wasm32-unknown-unknown
-}
-
 function install_wasm2wat() {
     cd /tmp
     git clone https://github.com/WebAssembly/wabt
@@ -20,6 +16,5 @@ function install_wasm_gc() {
     cargo install wasm-gc
 }
 
-(install_wasm_target)
 (install_wasm2wat)
 (install_wasm_gc)
