8f62464ab9cde7081e171904dae1bcf9bae87db0
Jake Goulding <jake.goulding@integer32.com>
2023-10-30 22:08:58 -0400

    Extend buttons to have a smaller grouping

--- ui/frontend/ButtonSet.tsx
+++ ui/frontend/ButtonSet.tsx
@@ -5,23 +5,35 @@ import Link, { LinkProps } from './uss-router/Link';
 import styles from './ButtonSet.module.css';
 
 interface ButtonSetProps {
+  className?: string;
   children: React.ReactNode;
 }
 
-export const ButtonSet: React.FC<ButtonSetProps> = ({ children }) => (
-  <div className={styles.set}>{children}</div>
+export const ButtonSet: React.FC<ButtonSetProps> = ({ className = '', children }) => (
+  <div className={`${styles.set} ${className}`}>{children}</div>
 );
 
 type HTMLButton = JSX.IntrinsicElements['button'];
 
 interface ButtonProps extends HTMLButton {
   isPrimary?: boolean;
+  isSmall?: boolean;
   iconLeft?: React.FC;
   iconRight?: React.FC;
 }
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ isPrimary = false, iconLeft: IconLeft, iconRight: IconRight, children, ...props }, ref) => {
+  (
+    {
+      isPrimary = false,
+      isSmall = false,
+      iconLeft: IconLeft,
+      iconRight: IconRight,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
     const iconLeft = IconLeft && (
       <span className={styles.iconLeft}>
         <IconLeft />
@@ -33,9 +45,10 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       </span>
     );
     const ordinalStyle = isPrimary ? styles.primary : styles.secondary;
+    const smallStyle = isSmall ? styles.small : '';
 
     return (
-      <button ref={ref} className={ordinalStyle} {...props}>
+      <button ref={ref} className={`${ordinalStyle} ${smallStyle}`} {...props}>
         {iconLeft}
         {children}
         {iconRight}
@@ -47,12 +60,20 @@ Button.displayName = 'Button';
 
 export const Rule: React.FC = () => <span className={styles.rule} />;
 
-export const IconButton = React.forwardRef<HTMLButtonElement, HTMLButton>(
-  ({ children, ...props }, ref) => (
-    <button ref={ref} className={styles.icon} {...props}>
-      {children}
-    </button>
-  ),
+interface IconButtonProps extends HTMLButton {
+  isSmall?: boolean;
+}
+
+export const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ isSmall = false, children, ...props }, ref) => {
+    const smallStyle = isSmall ? styles.small : '';
+
+    return (
+      <button ref={ref} className={`${styles.icon} ${smallStyle}`} {...props}>
+        {children}
+      </button>
+    );
+  },
 );
 IconButton.displayName = 'IconButton';
 
