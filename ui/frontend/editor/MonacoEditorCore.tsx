9352eeb0af3bbb7e7a07dd0721b8ce8bfd59a236
Jake Goulding <jake.goulding@integer32.com>
2024-10-01 15:32:53 -0400

    Offer crate autocomplete for the Monaco editor

--- ui/frontend/editor/MonacoEditorCore.tsx
+++ ui/frontend/editor/MonacoEditorCore.tsx
@@ -1,7 +1,8 @@
 import * as monaco from 'monaco-editor';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 import { useAppSelector } from '../hooks';
+import { offerCrateAutocompleteOnUse } from '../selectors';
 import { CommonEditorProps } from '../types';
 import { themeVsDarkPlus } from './rust_monaco_def';
 
@@ -14,7 +15,7 @@ function useEditorProp<T>(
     editor: monaco.editor.IStandaloneCodeEditor,
     model: monaco.editor.ITextModel,
     prop: T,
-  ) => void,
+  ) => void | (() => void),
 ) {
   useEffect(() => {
     if (!editor) {
@@ -26,13 +27,15 @@ function useEditorProp<T>(
       return;
     }
 
-    whenPresent(editor, model, prop);
+    return whenPresent(editor, model, prop);
   }, [editor, prop, whenPresent]);
 }
 
 const MonacoEditorCore: React.FC<CommonEditorProps> = (props) => {
   const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
   const theme = useAppSelector((s) => s.configuration.monaco.theme);
+  const completionProvider = useRef<monaco.IDisposable | null>(null);
+  const autocompleteOnUse = useAppSelector(offerCrateAutocompleteOnUse);
 
   // Replace `initialCode` and `initialTheme` with an "effect event"
   // when those stabilize.
@@ -96,6 +99,60 @@ const MonacoEditorCore: React.FC<CommonEditorProps> = (props) => {
 
   useEditorProp(editor, theme, (editor, _model, theme) => {
     editor.updateOptions({ theme });
+  });
+
+  const autocompleteProps = useMemo(
+    () => ({ autocompleteOnUse, crates: props.crates }),
+    [autocompleteOnUse, props.crates],
+  );
+
+  useEditorProp(editor, autocompleteProps, (_editor, _model, { autocompleteOnUse, crates }) => {
+    completionProvider.current = monaco.languages.registerCompletionItemProvider('rust', {
+      triggerCharacters: [' '],
+
+      provideCompletionItems(model, position, _context, _token) {
+        const word = model.getWordUntilPosition(position);
+
+        function wordBefore(
+          word: monaco.editor.IWordAtPosition,
+        ): monaco.editor.IWordAtPosition | null {
+          const prevPos = { lineNumber: position.lineNumber, column: word.startColumn - 1 };
+          return model.getWordAtPosition(prevPos);
+        }
+
+        const preWord = wordBefore(word);
+        const prePreWord = preWord && wordBefore(preWord);
+
+        const oldStyle = prePreWord?.word === 'extern' && preWord?.word === 'crate';
+        const newStyle = autocompleteOnUse && preWord?.word === 'use';
+
+        const triggerPrefix = oldStyle || newStyle;
+
+        if (!triggerPrefix) {
+          return { suggestions: [] };
+        }
+
+        const range = {
+          startLineNumber: position.lineNumber,
+          endLineNumber: position.lineNumber,
+          startColumn: word.startColumn,
+          endColumn: word.endColumn,
+        };
+
+        const suggestions = crates.map(({ name, version, id }) => ({
+          kind: monaco.languages.CompletionItemKind.Module,
+          label: `${name} (${version})`,
+          insertText: `${id}; // ${version}`,
+          range,
+        }));
+
+        return { suggestions };
+      },
+    });
+
+    return () => {
+      completionProvider.current?.dispose();
+    };
   });
 
   return <div className={styles.monaco} ref={child} />;
