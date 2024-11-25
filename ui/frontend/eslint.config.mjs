1a3ce1aa8928378f134f552c47fb873f72830510
Jake Goulding <jake.goulding@integer32.com>
2024-11-25 10:56:13 -0500

    Upgrade ESLint to 9.15

---
+++ ui/frontend/eslint.config.mjs
@@ -0,0 +1,69 @@
+// @ts-check
+import { fixupPluginRules } from '@eslint/compat';
+import eslint from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+
+  {
+    plugins: {
+      'react-hooks': fixupPluginRules(reactHooksPlugin),
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'no-restricted-syntax': [
+        'error',
+        {
+          message: 'Use `useAppDispatch` instead',
+          selector: 'CallExpression[callee.name="useDispatch"]',
+        },
+        {
+          message: 'Use `useAppSelector` instead',
+          selector: 'CallExpression[callee.name="useSelector"]',
+        },
+      ],
+
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/no-use-before-define': [
+        'error',
+        {
+          functions: false,
+          variables: false,
+        },
+      ],
+
+      'react/jsx-boolean-value': ['error', 'never'],
+
+      ...reactHooksPlugin.configs.recommended.rules,
+
+      // Disabled to upgrade to eslint 9
+      'no-empty': 'off',
+      'no-useless-escape': 'off',
+      'no-fallthrough': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+    },
+  },
+);
