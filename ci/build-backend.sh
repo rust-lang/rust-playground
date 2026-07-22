2fe8e239aaa33db62f48af618506c789259cc265
Jake Goulding <jake.goulding@integer32.com>
2026-07-22 11:00:13 -0400

    Use `docker build --output` instead of `docker cp`

--- ci/build-backend.sh
+++ ci/build-backend.sh
@@ -5,12 +5,9 @@ set -eu
 IMAGE_NAME=backend-build
 OUTPUT_DIR=docker-output
 
-docker build -t "${IMAGE_NAME}" -f ci/Dockerfile .
-
-mkdir -p "${OUTPUT_DIR}"
-
-container_id=$(docker create "${IMAGE_NAME}")
-for f in unit_tests_orchestrator unit_tests_ui ui; do
-    docker cp "${container_id}:/output/${f}" "${OUTPUT_DIR}"
-done
-docker rm -f "${container_id}"
+docker build \
+       --tag "${IMAGE_NAME}" \
+       --file ci/Dockerfile \
+       --target output \
+       --output "${OUTPUT_DIR}" \
+       .
